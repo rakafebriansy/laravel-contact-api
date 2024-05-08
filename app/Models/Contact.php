ffe89bb6062f2d6700c51837db4924a2d49dc2a3
rakafebriansy <kerenr445@gmail.com>
2024-05-08 13:39:44 +0700

    chore: adding contact model

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -4,17 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class User extends Model
+class Contact extends Model
 {
-    protected $table = 'users';
+    protected $table = 'contacts';
     protected $primaryKey = 'id';
     protected $keyType = 'int';
     public $timestamps = true;
     public $incrementing = true;
-    public function contacts(): HasMany
+
+    public function user(): BelongsTo
     {
-        return $this->hasMany(Contact::class,'user_id','id');
+        return $this->belongsTo(User::class,'user_id','id');
     }
 }
