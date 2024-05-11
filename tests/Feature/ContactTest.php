72c8197438dc482b43112864bbc68514444b38ff
rakafebriansy <kerenr445@gmail.com>
2024-05-11 07:46:39 +0700

    feat: Update Contact API

--- tests/Feature/ContactTest.php
+++ tests/Feature/ContactTest.php
@@ -116,4 +116,45 @@ class ContactTest extends TestCase
             ]
         ]);
     }
+    public function testUpdateSuccess()
+    {
+        $this->seed([UserSeeder::class,ContactSeeder::class]);
+        $contact = Contact::limit(1)->first();
+        $this->put('/api/contacts/' . $contact->id,
+        [
+            'first_name' => 'test2',
+            'last_name' => 'test2',
+            'email' => 'test2@example.com',
+            'phone' => '111112'
+        ],
+        [
+            'Authorization' => 'test'
+        ])->assertStatus(200)->assertJson([
+            'data' => [
+                'first_name' => 'test2',
+                'last_name' => 'test2',
+                'email' => 'test2@example.com',
+                'phone' => '111112'
+            ]
+        ]);
+    }
+    public function testUpdateValidationError()
+    {
+        $this->seed([UserSeeder::class,ContactSeeder::class]);
+        $contact = Contact::limit(1)->first();
+        $this->put('/api/contacts/' . $contact->id,
+        [
+            'first_name' => '',
+            'last_name' => 'test2',
+            'email' => 'test2@example.com',
+            'phone' => '111112'
+        ],
+        [
+            'Authorization' => 'test'
+        ])->assertStatus(400)->assertJson([
+            'errors' => [
+                'first_name' => ['The first name field is required.'],
+            ]
+        ]);
+    }
 }
